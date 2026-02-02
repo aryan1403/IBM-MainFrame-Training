8edbbb3d3e91a70ae6d6125f246d92509f5a3a3d
aryan1403 <aaryan14032006@email.com>
2026-02-02 12:16:29 +0530

    basics of node js

---
+++ Day2/index.js
@@ -0,0 +1,38 @@
+let a = 5;
+let b = 10;
+let c = 'hello world';
+
+// console.log('Sum:', a + b);
+// console.log('a * b:', a * b);
+// console.log('a / b:', a / b);
+// console.log(c);
+
+
+// if(a > b) {
+    //     console.log('a is greater than b');
+    // } else {
+        //     console.log('a is not greater than b');
+        // }
+        
+let arr = [1, 'Aryan', 3, 4, -1, true];
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//     console.log(element);
+// }
+
+// for(let e of arr) {
+//     console.log(e);
+// }
+// arr.forEach((e, index) => console.log(index, e));
+// arr.filter(e => typeof e === 'string').forEach(e => console.log(e));
+ 
+
+// arr -> mul num by 2
+
+// arr.filter(e => typeof e === 'number')
+
+// arr.filter(e => typeof e === 'number').sort().forEach(e => console.log(e));
+// .map(e => e * 2)
+// .forEach(e => console.log(e));
